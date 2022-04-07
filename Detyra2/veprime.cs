1e466135ac095c61fa8a5af6460b698f74f3cf9b
klaudia1786 <64419444+klaudia1786@users.noreply.github.com>
2022-04-07 15:11:38 +0200

    Update veprime.cs

--- Detyra2/veprime.cs
+++ Detyra2/veprime.cs
@@ -1,1 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace tek
+{
+    internal class Class1
+    {
+
+        static void Main(string[] args)
+        {
+
+            
+            /*Krijoni nje program i cili tregon nese nje numer i plote i dhene nga perdoruesi eshte tek ose cift. 
+             Ne rast se perdoruesi nuk ka vendosur nje numer, te afishohet mesazhi perkates.*/
+
+
+           Console.WriteLine("Ju lutem vendoni nje numer ");
+
+            string strInput ;
+           
+            int number;
+
+           if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number % 2 == 0)
+                {
+
+                    Console.WriteLine("Numri i dhene eshte cift");
+                  }
+                else
+                {
+                    Console.WriteLine("Numri i dhene eshte tek");
+                }
+            }
+            else
+                Console.WriteLine(" Variabli nuk eshte ne formatin int ");
+        }
+    }
+}
+                
